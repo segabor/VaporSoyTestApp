b5dad149f40bcdbfc587f86f9eab2280297ee330
Gábor Sebestyén <segabor@gmail.com>
2018-09-17 21:35:47 +0200

    Upgrade to Vapor 3

--- Package.swift
+++ Package.swift
@@ -1,26 +1,21 @@
 // swift-tools-version:4.0
-
 import PackageDescription
 
 let package = Package(
-    name: "VaporSoyTestApp",
-    products: [
-        .library(name: "App", targets: ["App"]),
-        .executable(name: "Run", targets: ["Run"])
-    ],
+    name: "VaporTest",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.2.0")),
-        .package(url: "https://github.com/vapor/leaf-provider.git", .upToNextMajor(from: "1.1.0")),
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+
+        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+
         .package(url: "https://github.com/segabor/Soy.git", .branch("master"))
     ],
     targets: [
-        .target(
-            name: "App",
-            dependencies: ["Vapor", "LeafProvider", "Soy"],
-            exclude: ["Config", "Database", "Public", "Resources"]
-        ),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Soy"], exclude: ["Config", "Database", "Public", "Resources"]),
         .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
+        .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
 
