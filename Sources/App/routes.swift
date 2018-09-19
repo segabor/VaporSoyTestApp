6cb72fb5ed158367ac317461e58cd8c85439718b
Gábor Sebestyén <segabor@gmail.com>
2018-09-19 09:01:07 +0200

    Add 'examples.simple.helloName' example

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,9 +1,15 @@
 import Vapor
+import Leaf
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
     router.get("hello") { req in
         return "Hello, world!"
+    }
+
+    // JavaScript examples
+    router.get("js", "simple") { req in
+        return try req.view().render("simple", ["name": "John Doe"])
     }
 }
