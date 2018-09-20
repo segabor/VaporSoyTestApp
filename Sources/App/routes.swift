48abd99d2da87cdeef527a6dc40090a57bb0c3e1
Gábor Sebestyén <segabor@gmail.com>
2018-09-20 04:51:19 +0200

    Examples to render templates on client-side is almost complete

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,12 +4,18 @@ import Leaf
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.get() { req in
+        return try req.view().render("index")
     }
 
     // JavaScript examples
-    router.get("js", "simple") { req in
-        return try req.view().render("simple", ["name": "John Doe"])
+    router.get("js", "simple", "helloWorld") { req in
+        return try req.view().render("js_simple/helloWorld")
+    }
+    router.get("js", "simple", "helloName") { req in
+        return try req.view().render("js_simple/helloName", ["name": "John Appleseed"])
+    }
+    router.get("js", "simple", "helloNames") { req in
+        return try req.view().render("js_simple/helloNames", ["name": "John Appleseed"])
     }
 }
